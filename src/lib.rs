bf4ccd08ad06e117dcd7142e963244dbd542a7f3
Wenfeng Wang <kalot.wang@gmail.com>
2020-10-08 20:31:10 +0800

    Disable doc lint on rpc definition

--- src/lib.rs
+++ src/lib.rs
@@ -170,6 +170,7 @@ impl<T: Runtime> ClientBuilder<T> {
 
 /// Client to interface with a substrate node.
 pub struct Client<T: Runtime> {
+    /// shut up
     pub rpc: Rpc<T>,
     genesis_hash: T::Hash,
     metadata: Metadata,
